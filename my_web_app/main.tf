d45ad307870dd61dce329c67f6ed4b0adfa15e8d
Kalen Peterson <kalen.peterson@cdw.com>
2023-05-24 18:37:22 -0700

    First attempt at adding zone/cidr rule support

--- my_web_app/main.tf
+++ my_web_app/main.tf
@@ -1,8 +1,5 @@
 # Create WEB Rules for this APP with Module 
 module "aws_sg_web" {
   source = "../modules/aws_sg_web"
-
-  vpc_id            = ""
-  ingress_cidr_ipv4 = "10.0.0.0/8"
-  egress_cidr_ipv4  = "10.0.0.0/8"
+  vpc_id = ""
 }
